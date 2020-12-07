74e97d1a7c18397b4dd54e06001288b32ca0c827
Tracy Golden <tracy80s2003@gmail.com>
2020-12-07 14:27:14 -0600

    updating javascript

--- script.js
+++ script.js
@@ -1,27 +1,141 @@
 // Variables for questions
-var questions = [{
-    question: "1. Commonly used data types DO NOT include _____;",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    correctAnswer: 2
-}, {
-    question: "2. The condition in an if/else statement is enclosed within _____;",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    correctAnswer: 2
-}, {
-    question: "3. Arrays in JavaScript can be used to store _____;",
-    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-    correctAnswer: 3
-}, {
-    question: "4. String values must be enclosed within _____ when being assigned to variables.",
-    choices: ["commas", "curly brackets", "quotes", "parentheses"],
-    correctAnswer: 2
-}, {
-    question: "5. A very useful tool used during development and debugging for printing content to the debugger is:;",
-    choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-    correctAnswer: 3
-}];
-
-var currentQuestion = 0;
-var viewingAns = 0;
-var correctAnswers = 0;
-var quizOver = false;
+var myQuestions = [
+    {
+        question: "What does HTML stand for?",
+        answers: {
+            a: 'Hyper Tracy Mush Langauge',
+            b: 'Hyper Text Processor Langauge',
+            c: 'Hyper Text Markup Langauge',
+            d: 'Hyper Text Main Langauge'
+    },
+        correctAnswer: 'c'
+    },
+    {
+        question: "What letters in a tag are used to define a list item (in a bulleted list)?",
+        answers: {
+            a: 'ul',
+            b: 'u',
+            c: 's',
+            d: 'li'
+    },
+        correctAnswer: 'd'
+    },
+    {
+        question: "What is the name of the object that allows you to perform mathematical tasks with the interpreter?",
+        answers: {
+            a: 'Solve',
+            b: 'Count',
+            c: 'Number',
+            d: 'Math'
+        },
+        correctAnswer: 'd'
+    },
+    {
+        question: "In JavaScript, what element is used to store multiple values in a single variable?",
+        answers: {
+            a: 'Arrays',
+            b: 'Functions',
+            c: 'Strings',
+            d: 'Variables'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "CSS stands for ____ Style Sheets.",
+        answers: {
+            a: 'Conceptual',
+            b: 'Cascading',
+            c: 'Curious',
+            d: 'Clap-worthy'
+        },
+        correctAnswer: 'b'
+    }
+  ];
+  
+  var quizContainer = document.getElementById('quiz');
+  var resultsContainer = document.getElementById('results');
+  var submitButton = document.getElementById('submit');
+  
+  generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+  
+  function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+  
+    function showQuestions(questions, quizContainer){
+      // we'll need a place to store the output and the answer choices
+      var output = [];
+      var answers;
+  
+      // for each question...
+      for(var i=0; i<questions.length; i++){
+        
+        // first reset the list of answers
+        answers = [];
+  
+        // for each available answer...
+        for(letter in questions[i].answers){
+  
+          // ...add an html radio button
+          answers.push(
+            '<label>'
+              + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+              + letter + ': '
+              + questions[i].answers[letter]
+            + '</label>'
+          );
+        }
+  
+        // add this question and its answers to the output
+        output.push(
+          '<div class="question">' + questions[i].question + '</div>'
+          + '<div class="answers">' + answers.join('') + '</div>'
+        );
+      }
+  
+      // finally combine our output list into one string of html and put it on the page
+      quizContainer.innerHTML = output.join('');
+    }
+  
+  
+    function showResults(questions, quizContainer, resultsContainer){
+      
+      // gather answer containers from our quiz
+      var answerContainers = quizContainer.querySelectorAll('.answers');
+      
+      // keep track of user's answers
+      var userAnswer = '';
+      var numCorrect = 0;
+      
+      // for each question...
+      for(var i=0; i<questions.length; i++){
+  
+        // find selected answer
+        userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+        
+        // if answer is correct
+        if(userAnswer===questions[i].correctAnswer){
+          // add to the number of correct answers
+          numCorrect++;
+          
+          // color the answers green
+          answerContainers[i].style.color = 'lightgreen';
+        }
+        // if answer is wrong or blank
+        else{
+          // color the answers red
+          answerContainers[i].style.color = 'red';
+        }
+      }
+  
+      // show number of correct answers out of total
+      resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+    }
+  
+    // show questions right away
+    showQuestions(questions, quizContainer);
+    
+    // on submit, show results
+    submitButton.onclick = function(){
+      showResults(questions, quizContainer, resultsContainer);
+    }
+  
+  }
